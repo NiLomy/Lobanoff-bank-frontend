7596804d4c76ac245d8cb0a14b9da033394ae768
Nikita Lobanov <lobanov.nikita2018@yandex.ru>
2024-05-15 20:33:54 +0300

    add api, accounts and settings

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BACKEND = "http://localhost:8081";
+const BACKEND = "http://localhost:8080";
 
 export const auth = axios.create({
-  baseURL: BACKEND + "/api/v1/auth",
+  baseURL: BACKEND + "core/api/v1/auth",
 });
